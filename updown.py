c065d08d6f44787aa99a26a461e394c5bd076d38
kuny092 <kokun12@naver.com>
2023-01-09 16:20:50 +0900

    기능구현 완료

--- updown.py
+++ updown.py
@@ -1,61 +1,65 @@
 from random import*
-class Updown:
-        def udgame():
-                tries=0 #시도횟수
-                n = 5 #기회 5번
-                number = randint(1,100) #난수 생성
-                print ("""____/                                     ____        __                        __       __      __      __    
-                        /\  _`\ \                                  /\  _`\     /\ \__                    /\ \__   /\ \    /\ \    /\ \    
-                        \\ \ \L\_\    __       ___ ___       __   \ \,\L\_\    \ \ ,_\     __      _ __  \ \ ,_\  \ \ \   \ \ \   \ \ \   
-                        \\ \ \L_L   /'__`\   /' __` __`\   /'__`\  \/_\__ \    \ \ \/   /'__`\   /\`'__\ \ \ \/   \ \ \   \ \ \   \ \ \  
-                        \\ \ \/, \/\ \L\.\_ /\ \/\ \/\ \ /\  __/    /\ \L\ \   \ \ \_ /\ \L\.\_ \ \ \/   \ \ \_   \ \_\   \ \_\   \ \_\ 
-                            \\ \____/\ \__/.\_\\ \_\ \_\ \_\\ \____\   \ `\____\   \ \__\\ \__/.\_\ \ \_\    \ \__\   \/\_\   \/\_\   \/\_\
-                            \\/___/  \/__/\/_/ \/_/\/_/\/_/ \/____/    \/_____/    \/__/ \/__/\/_/  \/_/     \/__/    \/_/    \/_/    \/_/ 
-                    """)
+playerList = ['홍길동','유재석','박지성']
+userName = ['홍길동']
+
+# 세명이 돌아야됨 -> 인덱스 0,1,2 -> 0,1,2
+def udgame(lastLoser):
+    up = 100
+    down = 1
+    index = playerList.index(lastLoser)
+    if index == 0:
+        while True:
+            number = int(input("친구들이 맞출 숫자를 입력해주세요 : "))
+            if number < 1 or number > 100:
+                print("1부터 100까지의 수를 입력해주세요.")
+            else:
+                break
+    else:
+        number = randint(down, up) #난수 생성
+        
+    tries=0 #시도횟수
+    n = 8 #기회 8번
+
+    while tries < n: #시도횟수가 5번이 될때까지 반복
+        try:
+            player = playerList[index] # index번째
+            if player == lastLoser:
+                index += 1                
+                if index == len(playerList):
+                    index = 0
+                continue
+            else:
+                if index == 0:
+                    guess = int(input("숫자를 짐작하세요 : "))
+                else:
+                    guess = randint(down, up)
+                    print('{} : {}'.format(player, guess))
+                tries += 1
+            
+                if guess != number:
+                    if number < guess:
+                        print ("땡 ~ DOWN!!!")
+                        up = guess -1
+                    if number > guess:
+                        print ("땡 ~ UP!!!")
+                        down = guess + 1
+                else:
+                    print ("정답!! 문제를 낸 사람이 벌주를 마십니다!")
+                    return lastLoser
+            
+                if tries == n:
+                    print ("정답은 {0}이었습니다!".format(number))
+                    print ("기회를 다 썼습니다!! 못맞추신 분들 벌주 마시세요~~~")
+                    lastLoser = playerList[1:]
+                    return lastLoser
+            
+                index += 1                
+                if index == len(playerList):
+                    index = 0
                 
-                while tries <= n: #시도횟수가 5번이 될때까지 반복
-                    try:                                                                                                                                              
-                        guess = int(input('\n 숫자를 맞춰보세요!!:')) #
-                        tries += 1
-                        if guess == number:
-                                print ("""  ____                                      _____                               __      __      __     
-                                            /\  _`\                                   /\  __`\                            /\ \    /\ \    /\ \    
-                                            \ \ \L\_\     __       ___ ___       __   \ \ \/\ \    __  __     __    _ __  \ \ \   \ \ \   \ \ \   
-                                            \ \ \L_L   /'__`\   /' __` __`\   /'__`\  \ \ \ \ \  /\ \/\ \  /'__`\ /\`'__\ \ \ \   \ \ \   \ \ \  
-                                                \ \ \/, \/\ \L\.\_ /\ \/\ \/\ \ /\  __/   \ \ \_\ \ \ \ \_/ |/\  __/ \ \ \/   \ \_\   \ \_\   \ \_\ 
-                                                \ \____/\ \__/.\_\\ \_\ \_\ \_\\ \____\   \ \_____\ \ \___/ \ \____\ \ \_\    \/\_\   \/\_\   \/\_\
-                                                \/___/  \/__/\/_/ \/_/\/_/\/_/ \/____/    \/_____/  \/__/   \/____/  \/_/     \/_/    \/_/    \/_/ """)
-                                print ("정답!! 문제를 낸 사람이 벌주를 마십니다!")
-                                break
-                        elif number < guess:
-                            print ("땡 ~ DOWN!!!")
-                        elif number > guess:
-                            print ("땡 ~ UP!!!")
-                    except:
-                            print ("숫자를 입력하셔야합니다!!")
-                if tries > n and not guess==number:
-                                print ("""  ____                                      _____                               __      __      __     
-                                            /\  _`\                                   /\  __`\                            /\ \    /\ \    /\ \    
-                                            \ \ \L\_\     __       ___ ___       __   \ \ \/\ \    __  __     __    _ __  \ \ \   \ \ \   \ \ \   
-                                            \ \ \L_L   /'__`\   /' __` __`\   /'__`\  \ \ \ \ \  /\ \/\ \  /'__`\ /\`'__\ \ \ \   \ \ \   \ \ \  
-                                                \ \ \/, \/\ \L\.\_ /\ \/\ \/\ \ /\  __/   \ \ \_\ \ \ \ \_/ |/\  __/ \ \ \/   \ \_\   \ \_\   \ \_\ 
-                                                \ \____/\ \__/.\_\\ \_\ \_\ \_\\ \____\   \ \_____\ \ \___/ \ \____\ \ \_\    \/\_\   \/\_\   \/\_\
-                                                \/___/  \/__/\/_/ \/_/\/_/\/_/ \/____/    \/_____/  \/__/   \/____/  \/_/     \/_/    \/_/    \/_/ """)
-                                print ("정답은 {0}이었습니다!".format(number))
-                                print ("기회를 다 썼습니다!! 못맞추신 분들 벌주 마시세요~~~")
+        except Exception as e:
+            print ("숫자를 입력하셔야합니다!!", e)
+        
+
             
-            
-
-        
-        
-
-
-        
-              
-
-        
-        
-
-
-        
-  
+print(udgame('유재석'))
